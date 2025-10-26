11f58e2f672e7eb3db956c5b10fa7b201476f1ed
7Bssk7 <arhip.fin3@gmail.com>
2025-10-26 01:08:04 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -32,6 +32,20 @@ int main() {
 
 
 
+
     return 0;
+}
+
+int main_menu(){
+    int c;
+    cout << "*** GOAT MANAGER 3001 ***" << endl;
+    cout << "[1] Add a goat"<< endl;
+    cout << "[2] Delete a goat" << endl;
+    cout << "[3] List goats" << endl;
+    cout << "[4] Quit" << endl;
+    cout << "Choice -->";
+    cin >> c;
+
+    return c;
 }
 
